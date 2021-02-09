48233e9b90ddadda5028c500f6c8f231cfaeb8f8
hataneko <hataneko.rxc@gmail.com>
2021-02-09 22:41:29 +0900

    第1部 第5章 原則をあえて破るとき

---
+++ tdd-study/tdd-study/Franc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_study
+{
+    public class Franc
+    {
+        private int amount;
+
+        public Franc(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public Franc Times(int multiplier)
+        {
+            return new Franc(this.amount * multiplier);
+        }
+
+        public override bool Equals(Object obj)
+        {
+            Franc dollar = (Franc)obj;
+            return this.amount == dollar.amount;
+        }
+    }
+}
